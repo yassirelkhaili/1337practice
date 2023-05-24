e6afe5570b13369410db6bd546086f90246a6f00
yassirelkhaili <elkhailiyassir@gmail.com>
2023-05-24 21:26:39 +0100

    added ex05

---
+++ C02/ex05.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int ft_str_is_lowercase(char *str);
+
+int ft_str_is_lowercase(char *str) {
+if (str[0] == '\0') {
+    return 1; 
+}
+for(int i = 0; str[i] != '\0'; i++) {
+    if(str[i] > 'A' && str[i] < 'Z') {
+        return 1; 
+    } 
+} 
+return 0; 
+}
+
+int main(int argc, char const *argv[])
+{
+    char string[] = "U"; 
+    printf("%i", ft_str_is_lowercase(string)); 
+    return 0;
+}
