1b59f3b1817fca68f6f7e6c5de87c413a2e7f5d0
yassirelkhaili <elkhailiyassir@gmail.com>
2023-05-28 17:44:26 +0100

    added special ex

---
+++ c04/ex04.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//EXCERRCIE I DID NOT INCLUDED IN THE PISCINE EXCERCICES CHECK IF 2 ARRAYS ARE EQUAL
+int ft_array(char* dest, char* src); 
+
+int ft_array(char* dest, char* src) {
+char *ptr1 = dest;
+unsigned int destlen = 0; 
+while(*ptr1 != '\0')  {
+    ptr1++; 
+    destlen++; 
+} 
+char *ptr2 = src;
+unsigned int srclen = 0; 
+while(*ptr2 != '\0')  {
+    ptr2++; 
+    srclen++; 
+} 
+if (srclen == destlen) {
+while(*dest == *src) {
+    dest++; 
+    src++; 
+    if (*src == '\0') {
+    return 1; 
+}
+}
+} 
+return 0; 
+}
+
+int main(int argc, char const *argv[])
+{
+    char dest[] = "Hello World"; 
+    char src[] = "Hello World"; 
+    printf("%i", ft_array(dest, src));  
+    return 0;
+}
