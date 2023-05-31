96c25dfb3429d6e037fd71be91a4d8fbb7b313f3
yassirelkhaili <elkhailiyassir@gmail.com>
2023-05-31 09:35:54 +0100

    added another special ex00

--- c04/c.c
+++ c04/c.c
@@ -10,17 +10,17 @@ int main(int argc, char *argv[])
     char *arg = argv[3]; 
     char *to_find = argv[2]; 
     char *temp_ptr = str;
-    char *temp_ptr2 = str;  
+    char *temp_ptr2 = str; 
+     unsigned int strlen = 0; 
+    while(*temp_ptr2) {
+        temp_ptr2++; 
+        strlen++; 
+    } 
     while(*temp_ptr) {
         if(*temp_ptr == *to_find) {
             *temp_ptr = *arg; 
         }
         temp_ptr++; 
-    }
-    unsigned int strlen = 0; 
-    while(*temp_ptr2) {
-        temp_ptr2++; 
-        strlen++; 
     }
     write(STDOUT_FILENO, str, strlen); 
     return 0;
