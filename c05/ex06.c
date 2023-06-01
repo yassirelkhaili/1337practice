551a7a27b99b2c8fcc2f01210cf8e8cf1684b68c
yassirelkhaili <elkhailiyassir@gmail.com>
2023-06-01 12:07:29 +0100

    added ex06

--- c05/ex06.c
+++ c05/ex06.c
@@ -2,12 +2,16 @@
 
 int ft_is_prime(int nb);
 
-int ft_is_prime(int nb) {
-if (nb == 1 || nb == 0) {
+int ft_is_prime(int nb) 
+{
+if (nb == 1 || nb == 0) 
+{
     return 0; 
 }
-for(int i = 2; i < nb - 1; i++) {
-if(nb % i == 0) {
+for(int i = 2; i < nb - 1; i++) 
+{
+if(nb % i == 0) 
+{
     return 0; 
 }
 }
