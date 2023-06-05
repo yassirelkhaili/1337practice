06b150f0038acf179fcaaf1cff407a9bcf6b08d9
yassirelkhaili <elkhailiyassir@gmail.com>
2023-06-05 17:35:59 +0100

    added strcat

--- c08/strcat.c
+++ c08/strcat.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 
-char *my_strcpy(char *dest, const char *src);
+char *ft_strcpy(char *dest, const char *src);
 
-char *my_strcpy(char *dest, const char *src) {
+unsigned int ft_strlen(char* str); 
+
+char *ft_strcpy(char *dest, const char *src) {
     char *temp_ptr = dest; 
     while (*src) {
         *temp_ptr = *src;
@@ -12,10 +15,20 @@ char *my_strcpy(char *dest, const char *src) {
     *temp_ptr = '\0';
     return dest;
 }
-
+unsigned int ft_strlen(char* str) {
+    unsigned int strlen = 0; 
+    char *temp_ptr = str; 
+    while(*temp_ptr) {
+        strlen++; 
+        temp_ptr++; 
+    }
+    return strlen; 
+}
 int main(int argc, char const *argv[]) {
     char dest[20];
-    char src[] = "Hello World";
-    printf("%s", my_strcpy(dest, src));
+    char src[] = "Hello World!";
+    char*result = ft_strcpy(dest, src); 
+    unsigned int resultlen = ft_strlen(result); 
+    write(STDOUT_FILENO, result, resultlen);
     return 0;
 }
